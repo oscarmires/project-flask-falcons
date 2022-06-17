b6fa2e0dc5536133549b3fcb84cb725fb2b414cd
oscar-me <oscar-me@outlook.com>
2022-06-17 02:37:15 -0500

    Oscar's style

--- app/app.py
+++ app/app.py
@@ -15,15 +15,18 @@ def index():
 
 @app.route('/experience')
 def experience():
-    data_path = os.path.join(app.static_folder, 'data', 'sambina_experience.json')
+    data_path = os.path.join(app.static_folder, 'data', 'oscar_experience.json')
     job_list = json.load(open(data_path))
 
-    return render_template('work_experience.html', title="Work Experience", url=os.getenv("URL"), job_list=job_list)
+    return render_template('work_experience.html', title="Experience", url=os.getenv("URL"), job_list=job_list)
 
 
 @app.route('/hobbies')
 def hobbies():
-    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
+    data_path = os.path.join(app.static_folder, 'data', 'oscar_hobbies.json')
+    hobby_list = json.load(open(data_path))
+
+    return render_template('hobbies.html', title="Hobbies", hobby_list=hobby_list, url=os.getenv("URL"))
 
 
 @app.route('/education')
@@ -33,12 +36,11 @@ def education():
 
 @app.route('/places')
 def places():
-    countries = ["Canada", "Bangladesh", "India", "Thailand",
-                 "Singapore", "Malaysia", "Hong Kong", "Dubai", "Saudi Arabia"]
+    countries = ["Mexico", "United States", "Canada", "Germany", "Italy", "France", "Spain"]
     countries.sort()
     return render_template('places.html', title="Places Visited", url=os.getenv("URL"), countries=countries)
 
 
 @app.route('/projects')
 def projects():
-    return render_template('projects_and_skills.html', title="Technical Skills & Projects", url=os.getenv("URL"))
+    return render_template('projects_and_skills.html', title="Projects", url=os.getenv("URL"))
