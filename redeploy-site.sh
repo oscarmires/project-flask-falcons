6eec67c42a4e9e3fd1839e6754ddf4ed1118c714
Oscar Miranda <oscar-me@outlook.com>
2022-06-20 04:18:33 +0000

    add redeploy script

---
+++ redeploy-site.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+tmux kill-session -t portfolio-server
+cd project-flask-falcons
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+tmux new -d -s portfolio-server
+tmux send-keys "flask run --host=0.0.0.0" Enter
