b5c2936c9c546ed3a469919f3ffa4e0c60c97818
Oscar Miranda <oscar-me@outlook.com>
2022-07-14 01:26:14 +0000

    update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd project-flask-falcons
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
