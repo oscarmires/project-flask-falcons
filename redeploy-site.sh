1ae3a04eb2f731eb7883d9d1b6b287e1ef3eed5c
oscar-me <oscar-me@outlook.com>
2022-07-13 20:38:13 -0500

    add redeploy-site.sh script exception handling for cd command

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd project-flask-falcons
+cd project-flask-falcons || exit
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
